7613cc66a4a60b897f74905c94b3d14bf959db9e
TensorAdy <60922263+TensorAdy@users.noreply.github.com>
2020-07-08 11:29:03 +0200

    Add files via upload

---
+++ uild an Adversarial Game Playing Agent/my_custom_player.py
@@ -0,0 +1,95 @@
+
+from sample_players import DataPlayer
+from functools import reduce
+
+class CustomPlayer(DataPlayer):
+    """ Implement your own agent to play knight's Isolation
+
+    The get_action() method is the only required method for this project.
+    You can modify the interface for get_action by adding named parameters
+    with default values, but the function MUST remain compatible with the
+    default interface.
+
+    **********************************************************************
+    NOTES:
+    - The test cases will NOT be run on a machine with GPU access, nor be
+      suitable for using any other machine learning techniques.
+
+    - You can pass state forward to your agent on the next turn by assigning
+      any pickleable object to the self.context attribute.
+    **********************************************************************
+    """
+    def get_action(self, state):
+        """ Employ an adversarial search technique to choose an action
+        available in the current state calls self.queue.put(ACTION) at least
+
+        This method must call self.queue.put(ACTION) at least once, and may
+        call it as many times as you want; the caller will be responsible
+        for cutting off the function after the search time limit has expired.
+
+        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+
+        **********************************************************************
+        NOTE: 
+        - The caller is responsible for cutting off search, so calling
+          get_action() from your own code will create an infinite loop!
+          Refer to (and use!) the Isolation.play() function to run games.
+        **********************************************************************
+        """
+        # TODO: Replace the example implementation below with your own search
+        #       method by combining techniques from lecture
+        #
+        # EXAMPLE: choose a random move without any search--this function MUST
+        #          call self.queue.put(ACTION) at least once before time expires
+        #          (the timer is automatically managed for you)
+        #import random
+        #self.queue.put(random.choice(state.actions()))
+        import random
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.alpha_beta_search(state, depth=3))
+
+    def alpha_beta_search(self, state, depth):
+
+        def min_value(state, alpha, beta, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), alpha, beta, depth-1))
+                if value <= alpha:
+                    return value
+                beta = min(beta, value)
+            return value
+
+        def max_value(state, alpha, beta, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), alpha, beta, depth-1))
+                if value >= beta:
+                    alpha = max(alpha, value)
+            return value
+
+        def get_move(best_options, next_action):
+            (best_move, best_score, alpha) = best_options
+            value = min_value(state.result(next_action), alpha, float("-inf"), depth-1)
+            alpha = max(alpha, value)
+            return (next_action, value, alpha) if value >= best_score else (best_move, best_score, alpha)
+
+        return reduce(
+            get_move,
+            state.actions(),
+            (None, float("-inf"), float("-inf"))
+        )[0]
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        remaining_own_liberties = sum(len(state.liberties(l)) for l in own_liberties)
+        remaining_opp_liberties = sum(len(state.liberties(l)) for l in opp_liberties)
+        return len(own_liberties) - 3 * len(opp_liberties) + remaining_own_liberties - 3 * remaining_opp_liberties
